ec69be3db9e513b57e72a7005d250ef5373908d0
Qasim Rokeeb <qasimrokeeb@gmail.com>
2025-08-09 23:23:37 +0100

    updated code

--- contracts/SimpleSubscription.sol
+++ contracts/SimpleSubscription.sol
@@ -1,9 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-// =============================================================================
-// PROJECT 19: Simple Subscription - Monthly subscription payments
-// =============================================================================
 
 contract SimpleSubscription {
     struct Subscription {
